534b8e410e5b70ee24e867072237949b30813bf0
14817-ui <14817@holbertonstudents.com>
2026-08-19 02:17:21 +0000

    Fixing betty

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -5,15 +5,15 @@
  * string_nconcat - concatenates two strings
  * @s1: first string
  * @s2: second string
- * @n: number of bytes from s2 to use
+ * @n: number of bytes from s2
  *
- * Return: pointer to new string, or NULL if it fails
+ * Return: pointer to the new string, or NULL if it fails
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *result;
-	unsigned int len1;
-	unsigned int len2;
+	unsigned int len1 = 0;
+	unsigned int len2 = 0;
 	unsigned int i;
 
 	if (s1 == NULL)
@@ -21,34 +21,24 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	len1 = 0;
 	while (s1[len1] != '\0')
 		len1++;
 
-	len2 = 0;
 	while (s2[len2] != '\0')
 		len2++;
 
 	if (n > len2)
 		n = len2;
 
-	result = malloc(sizeof(char) * (len1 + n + 1));
+	result = malloc((len1 + n + 1) * sizeof(char));
 	if (result == NULL)
 		return (NULL);
 
-	i = 0;
-	while (i < len1)
-	{
+	for (i = 0; i < len1; i++)
 		result[i] = s1[i];
-		i++;
-	}
 
-	i = 0;
-	while (i < n)
-	{
+	for (i = 0; i < n; i++)
 		result[len1 + i] = s2[i];
-		i++;
-	}
 
 	result[len1 + n] = '\0';
 
